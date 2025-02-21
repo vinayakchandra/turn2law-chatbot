8f102bab5cd797449e808197ed22ea59f53b27a2
Vinayak Chandra Suryavanshi <vinayak.chandra.suryavanshi@gmail.com>
2025-02-21 20:07:19 +0530

    refactor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 __import__("pysqlite3")
-import os
 import sys
+
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+import os
 
 import streamlit as st
 from dotenv import load_dotenv
@@ -10,8 +12,6 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import Chroma
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
-
-sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
